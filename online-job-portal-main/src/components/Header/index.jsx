d52efae694a744df1a2b6be30e0c16f79bc38049
Keerthi Madugula <157406470+Madugula-Keerthi@users.noreply.github.com>
2025-08-23 16:14:09 +0530

    Update index.jsx

--- online-job-portal-main/src/components/Header/index.jsx
+++ online-job-portal-main/src/components/Header/index.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Header() {
   return (
     <div className='mt-10 flex flex-col gap-5 items-center justify-center text-white'>
-        <h1 className='text-5xl font-bold'>Your ideal job awaits, start the search</h1>
-        <p className='text-xl'>Get latest job openings that best suits you!</p>
+        <h1 className='text-5xl font-bold'>Find your next opportunity with confidence</h1>
+        <p className='text-xl'>Discover the latest openings from trusted employers.</p>
     </div>
   )
 }
